c171ebf556fc339e29727c4dff9cbfc014622493
Dr.Neil Watts <58720414+minue@users.noreply.github.com>
2023-06-02 18:10:18 +0900

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2153,7 +2153,7 @@ class MaskRCNN(object):
         metrics. Then calls the Keras compile() function.
         """
         # Optimizer object
-        optimizer = keras.optimizers.SGD(
+        optimizer = tf.keras.optimizers.legacy.SGD(
             learning_rate=learning_rate, momentum=momentum,
             clipnorm=self.config.GRADIENT_CLIP_NORM)
 
